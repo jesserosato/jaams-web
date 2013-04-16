92d10f1bdd7b5738004dc4434804447336c3f3e1
jesserosato <jesse.rosato@me.com>
2013-04-16 13:37:14 -0700

    Update Documentation

--- forms/models/JAAMSForms.model.php
+++ forms/models/JAAMSForms.model.php
@@ -1,5 +1,5 @@
 <?php
-// Make sure the base controller has been defined (also loads the base model).
+// Make sure the base controller has been defined and load the base model (see 'core/JAAMSBase.class.php').
 require_once(JAAMS_ROOT.'/core/JAAMSBase.class.php');
 
 class JAAMSForms_Model extends JAAMSBase_Model {
