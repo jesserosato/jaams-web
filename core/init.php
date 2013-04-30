f15f9fb8f88e29278b1ec25ad1782f7ef9247f8a
jesserosato <jesse.rosato@me.com>
2013-04-30 16:32:50 -0700

    Working commit.

---
+++ core/init.php
@@ -0,0 +1,5 @@
+<?php
+require_once('utilities/functions.php'); // For require_files.
+\JAAMS\Core\Utilities\require_files(dirname(__FILE__).'/utilities', '/\.class.php$/');
+\JAAMS\Core\Utilities\require_files(dirname(__FILE__).'/controllers', '/\.class.php$/');
+\JAAMS\Core\Utilities\require_files(dirname(__FILE__).'/models', '/\.model.php$/');
---
+++ forms/config.php
@@ -0,0 +1,3 @@
+<?php
+namespace Forms;
+define(__NAMESPACE__.'\ROOT', dirname(__FILE__));
